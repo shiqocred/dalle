a1ca686dc61cf85862ee68131ef3f0c407664057
shiqocred <21103080090@student.uin-suka.ac.id>
2023-01-29 03:03:41 +0700

    fourth commit

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true)
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('http://54.179.2.46/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type' : 'application/json'
@@ -46,7 +46,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('http://54.179.2.46/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type' : 'application/json'
